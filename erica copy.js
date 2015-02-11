529d8e020e120195253806fb0c10eadb4407517a
Patrick Moberg <patrickmoberg@gmail.com>
2015-02-11 12:57:23 -0800

    blog js update and image

---
+++ erica copy.js
@@ -0,0 +1,89 @@
+$( window ).resize(function() {
+  checkVideos();
+});
+
+function checkVideos() {
+  $("iframe").css("height", $("iframe").width() * (1080/1920) + "px");
+  $("#blogfeed iframe").css("height", 500 * (1080/1920) + "px");
+}
+
+$( document ).ready(function() {
+  
+  $("a[href='"+window.location.href+"']").addClass("current");
+  if(window.location.href == base_href) {
+    $(".header a:nth-of-type(2)").addClass("current");
+  }
+  
+  if (navigator.appVersion.indexOf("Chrome/") != -1) {
+    $("body").addClass("chrome");
+  }
+  
+  checkVideos();
+  
+  if(window.location.href.indexOf("localhost") > 0) {
+    tinymce.init({
+        selector: "h1,h2,div.editable",
+        inline: true,
+        plugins: [ "code link template" ],
+        toolbar: "bold italic styleselect | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link code",
+        menubar: false,
+        setup: function(editor) {
+          editor.on('change', function(e) {
+              var new_value = tinyMCE.activeEditor.getContent({format : 'raw'});
+              $.ajax({
+                type: "POST",
+                url: "../scripts/index.php",
+                data: {"id":parseInt(e.target.id), "key":$("#"+e.target.id).attr("type"), "v":JSON.stringify(new_value)},
+                success: function(e) {
+                
+                }
+              });
+          });
+        }
+    });
+    tinymce.init({
+        selector: ".element.editable",
+        inline: true,
+        menubar: false,
+        plugins: [
+            "advlist autolink lists link image charmap print preview anchor",
+            "searchreplace visualblocks code fullscreen",
+            "insertdatetime media table contextmenu code"
+        ],
+        toolbar: "bold italic styleselect | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link code",
+        setup: function(editor) {
+          editor.on('change', function(e) {
+              var new_value = tinyMCE.activeEditor.getContent({format : 'raw'});
+              $.ajax({
+                type: "POST",
+                url: "../scripts/index.php",
+                data: {"id":parseInt(e.target.id), "key":$("#"+e.target.id).attr("type"), "v":JSON.stringify(new_value)},
+                success: function(e) {
+  
+                }
+              });
+            
+          });
+        }
+    });
+  }
+  
+  
+  // $(".element").click(function(e) {
+  //   var image_container;
+  //   image_container = $("#image_container").length==1 ? $("#image_container") : $("<div></div>").attr("id","image_container");
+  //
+  //   var image;
+  //   if(image_container.find("img").length == 1) {
+  //     image = image_container.find("img");
+  //   }
+  //   else{
+  //     image = $("<img/>");
+  //     image_container.append(image);
+  //   }
+  //
+  //   image.attr("src",$(e.currentTarget).find("img").attr("src"));
+  //   $("iframe").after(image_container);
+  // });
+  // $(".element:nth-of-type(2)").click();
+});
--- tumblrfeed.js
+++ tumblrfeed.js
@@ -9,12 +9,23 @@ $(tumblr_api_read["posts"]).each(function(i,obj) {
   }
   
   if(obj["type"]=="photo") {
-    // console.log(obj);
-    var url = $("<a>").addClass("imgwrap").attr("href",obj["url-with-slug"]).attr("target","_blank");
-    var image = $("<img>").addClass("image").attr("src", obj["photo-url-500"]);
-    url.append(image);
-    var caption = $("<div>").addClass("image-caption").html(obj["photo-caption"]);
-    post.append(url).append(caption);
+    
+    var collection = [];
+    if(obj["photos"].length > 0) {
+      collection = obj["photos"];
+    }
+    else {
+      collection.push(obj);
+    }
+    
+    $.each(collection, function(sub_i,sub_obj) {
+      var url = $("<a>").addClass("imgwrap").attr("href",obj["url-with-slug"]).attr("target","_blank");
+      var image = $("<img>").addClass("image").attr("src", sub_obj["photo-url-500"]);
+      url.append(image);
+      var caption = $("<div>").addClass("image-caption").html(sub_obj["photo-caption"]);
+      post.append(url).append(caption);
+    });
+    
     $("#blogfeed").append(post);
   }
   
@@ -23,7 +34,7 @@ $(tumblr_api_read["posts"]).each(function(i,obj) {
     var div = $("<div></div>").html(obj["video-player-500"]);
     post.append(div);
     var caption = $("<div>").addClass("video-caption").html(obj["video-caption"]);
-    post.append(url).append(caption);
+    post.append(caption);
     $("#blogfeed").append(post);
   }
   
